a46ebe9d85cd3bf6ce0a6e3a701a7a17e5ab3a9d
cdelanoe <c.delanoe53@gmail.com>
2024-05-30 17:18:01 +0200

    Résolution du problème d'hydratation

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" suppressHydrationWarning className="h-full">
       <body
         className={cn(
           GeistSans.variable,
